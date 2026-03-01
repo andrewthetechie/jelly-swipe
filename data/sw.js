ec89deabb16092d9c8c67175e517ac24efe04f06
Bergasha <bergasha@hotmail.com>
2026-03-01 10:15:11 +1000

    Update sw.js remove notes

--- data/sw.js
+++ data/sw.js
@@ -6,7 +6,7 @@ self.addEventListener('activate', (event) => {
     event.waitUntil(clients.claim());
 });
 
-// Android Chrome REQUIRES a fetch handler to be considered a PWA
+
 self.addEventListener('fetch', (event) => {
     event.respondWith(fetch(event.request));
 });
