e21e2e9598da6cd8e3787961b0c990b077893ed1
syarnell <sarah.yarnell10@gmail.com>
2026-05-27 18:30:52 -0500

    feat: join room functionality
    can now join room with code
    also changed transparent background color on stacked cards that made details hard to read

--- frontend/JoinModal.jsx
+++ frontend/JoinModal.jsx
@@ -1,21 +1,40 @@
 import React from 'react'
 import { RoomContext } from './App'
+import { apiFetch } from "./api"
 
 export default function JoinModal({ onClose }) {
     const { currentRoomCode, setCurrentRoomCode } = React.useContext(RoomContext)
     const { userInputCode, setUserInputCode } = React.useContext(RoomContext)
+
+    async function joinRoom() {
+        try {
+            const res = await apiFetch(`/room/${userInputCode}/join`, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'}
+            })
+            if (res.ok) {
+                const data = await res.json()
+                setCurrentRoomCode(userInputCode)
+                console.log(`Joined room ${userInputCode}:`, data)
+            }
+        } catch (err) {
+            console.error("Error joining room:", err)
+        }
+    }
     return (
         <div className="join-modal modal">
             <h2>Enter Room Code</h2>
             <input 
                 type="text"
                 inputMode="numeric" 
+                minLength="4"
+                maxLength="4"
                 placeholder="Enter Host Code" 
                 className="room-code-input" 
                 value={userInputCode} 
                 onChange={(e) => setUserInputCode(e.target.value.replace(/[^0-9]/g, ''))} 
             />
-            <button className="modal-button">Join Session</button>
+            <button className="modal-button" onClick={joinRoom}>Join Session</button>
             <button className="modal-button" onClick={onClose} data-modal-type="join">Cancel</button>
         </div>
     )
