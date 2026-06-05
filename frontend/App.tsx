b2b55986382700e7155d5f27f8e79871e3c086cc
syarnell <sarah.yarnell10@gmail.com>
2026-06-05 16:58:24 -0500

    fix: removed old context provider code
    removed all references to the original context provider in App.tsx
    context has been moved to RoomContextProvider.tsx but some code remained

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,20 +3,6 @@ import Main from "./Main"
 import Header from "./Header"
 import { RoomContextProvider } from "./RoomContextProvider" 
 
-interface RoomContextType {
-    currentRoomCode: string | null
-    setCurrentRoomCode: React.Dispatch<React.SetStateAction<string | null>>
-    movies: boolean
-    setMovies: React.Dispatch<React.SetStateAction<boolean>>
-    tvShows: boolean
-    setTvShows: React.Dispatch<React.SetStateAction<boolean>>
-    isSoloMode: boolean
-    setIsSoloMode: React.Dispatch<React.SetStateAction<boolean>>
-    userInputCode: string
-    setUserInputCode: React.Dispatch<React.SetStateAction<string>>
-}
-const RoomContext = React.createContext<RoomContextType | undefined>(undefined)
-
 export default function App() {
 
 
@@ -28,4 +14,3 @@ export default function App() {
     )
 }
 
-export { RoomContext }
