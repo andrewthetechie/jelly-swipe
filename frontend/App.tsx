bcd398e111eceab13d01262793895d3debcbd5eb
syarnell <sarah.yarnell10@gmail.com>
2026-07-17 19:24:48 -0500

    feat: (App) authenticate jellyfin server
    useEffect calls fetch to /auth/jellfin-use-server-identity to confirm credentials

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,30 @@
 import React from "react"
 import Main from "./Main"
 import Header from "./Header"
+import { apiFetch } from "./api"
 import { RoomContextProvider } from "./RoomContextProvider" 
 import { SSEContextProvider } from "./SSEContextProvider"
 
 export default function App() {
+    const [auth, setAuth] = React.useState<boolean>(false)
+    React.useEffect(() => {
+        async function authBootstrap() {
+            try {
+                const res: Response = await apiFetch("/auth/jellyfin-use-server-identity", {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/json'},
+                })
+                if (!res.ok) {
+                    throw new Error(`Error authenticating server identity: ${res.status} ${res.statusText}`)
+                }
+                setAuth(true)
+            } catch (err) {
+                console.error("Error authenticating server identity:", err)
+            }
+        }
 
+        authBootstrap()
+    }, [])
 
     return (
         <RoomContextProvider>
