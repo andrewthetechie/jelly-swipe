192fd61d3e7525a3339985d0429353c91ce2903e
Sarah Yarnell <sarah.yarnell10@gmail.com>
2026-08-07 20:18:45 -0500

    fix(frontend): no longer ships ~4.3 MB of images on first load- #281
    Removed unused assets
    Resized name-logo.png and logo.png
    Moved to 5 icon schema for favicon
    Updated vite.config.js to account for maskable icon

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -21,8 +21,9 @@ export default defineConfig({
         background_color: "#111111",
         display: "standalone",
         icons: [
-          { src: "icon-192.png", sizes: "192x192", type: "image/png" },
-          { src: "icon-512.png", sizes: "512x512", type: "image/png" },
+          { src: "icon-192.png", type: "image/png", sizes: "192x192" },
+          { src: "icon-mask.png", type: "image/png", sizes: "512x512", purpose: "maskable" },
+          { src: "icon-512.png", type: "image/png", sizes: "512x512" },
         ],
       },
     }),
